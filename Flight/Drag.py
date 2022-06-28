0712234336ac641e3524d2ea254aa4eb6da45cc8
Barty Wardell <bjw68@cam.ac.uk>
2022-06-28 22:24:31 +0100

    Optimizer functional with errors (no interpolate)

--- Flight/Drag.py
+++ Flight/Drag.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 def Reynolds(length, velocity, viscosity = 1.5e-5):
+    #Calculates Reynolds number
     R = (length * velocity)/viscosity
     return R
 
 def R_crit(Roughness, Length):
+    #Finds critical Reynolds number for turbulence
     R_crit = 51 * (Roughness/Length)**(-1.039)
     return R_crit
 
 def friction_Re(Reynold, R_critical, Roughness, totatlen):
+    #Finds friction coefficent based on Reynolds number
     Coeff_friction = 0
     if Reynold <= 1e4:
         Coeff_friction = 1.48e-2
@@ -20,6 +23,7 @@ def friction_Re(Reynold, R_critical, Roughness, totatlen):
 
 
 def friction_vel(Coeff_friction, Machno, A_ref, fineness, A_body_wet, fin_thickness, A_fin_wet, MAC_base):
+    #Computes overall friction coefficent based on velocity
     Skin_friction_coeff = 0
     if Machno < 0.8:
         Skin_friction_coeff = Coeff_friction * (1 - ((0.1) * (Machno ** 2)))
@@ -35,6 +39,7 @@ def friction_vel(Coeff_friction, Machno, A_ref, fineness, A_body_wet, fin_thickn
 
 
 def Body_pressure_drag(Machno, maximumdiameter, noseconelen):
+    #Calculates body pressure drag
     eta = np.arctan(maximumdiameter/(2*noseconelen))
     Coeff_body_pressure = 0
     Coeff_body_pressure_subs = 0.8*(np.sin(eta))**2
@@ -51,6 +56,7 @@ def Body_pressure_drag(Machno, maximumdiameter, noseconelen):
     return Coeff_body_pressure
 
 def fin_pressure_drag(Machno, Leadingangle_base):
+    #Calculates fin pressure drag
     Coeff_fin_pressure_LE = 0  #Assuming a rounded leading edge
     if Machno < 0.9:
         Coeff_fin_pressure_LE = ((1 - (Machno**2))**(-0.417)) - 1
