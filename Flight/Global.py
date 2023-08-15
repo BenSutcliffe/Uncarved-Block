f7064370598eb200d958696abdbdc5e2105c809f
Barty Wardell <bjw68@cam.ac.uk>
2023-08-14 20:11:47 -0500

    Hi

--- Flight/Global.py
+++ Flight/Global.py
@@ -1,3 +1,4 @@
+#These lines are now complete rubbish but the code simulates descent - should be edited for most recent recovery system.
 cddrogue = 1  # Change this to alter the Cd of the drogue
 adrogue = 0.4 # Change this to alter the area of the drogue
 amain = 1.4 # Change this to alter the area of the main chute
@@ -7,7 +8,10 @@ mainheight = 1000  # Change this to alter the main chute deploy height
 droguedeploytime = 10  # Change this to alter the drogue deploy time
 maindeploytime = 10  # Change this to alter the main chute deploy time
 cddown = 0.4
+
+#Vehicle start and end mass
 start_mass = 29 #Aquila 29 kg
 end_mass = 18.7 #Aquila 18.7kg
 
+#Constant flight angle of attack for drag
 angle_attack = 5
